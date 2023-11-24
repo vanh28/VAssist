10c8ad985b781bf1e58b02e5abc4f9d54956e05d
Viet Anh Hoang <hoangvietanh8203@gmail.com>
2023-11-24 23:49:49 +0700

    Update Profile

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { MUINavBar } from '../components/MUINavBar'
+import SpeechReg from '../components/SpeechReg'
+function Profile() {
+  return (
+    <div >
+      <MUINavBar />
+      <SpeechReg />
+      Profile
+    </div>
+  )
+}
+
+export default Profile
