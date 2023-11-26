bc5b1f1a90979acde76d58af0192fdf0198e8d07
Viet Anh Hoang <hoangvietanh8203@gmail.com>
2023-11-27 01:27:40 +0700

    Update Videocall.jsx

--- src/pages/Videocall.jsx
+++ src/pages/Videocall.jsx
@@ -5,12 +5,25 @@ import axios from "axios";
 import {NewsContextProvider } from "../NewsContext";
 import Newspaper from "../components/Newspaper";
 import Container from '@mui/material/Container';
+import SpeechReg from '../components/SpeechReg';
 
 
 function Videocall() {
+  const handleAudio = () => { 
+    if (!speechSynthesis.speaking) {
+      let utterance = new SpeechSynthesisUtterance("This is a page for calling.");
+      utterance.lang = 'vi-VN';
+      speechSynthesis.speak(utterance);
+    }
+  }
+
+  useEffect(() => {
+    handleAudio();
+  }, []);
   return (
     <div className='Videocall'>
       <MUINavBar />
+      <SpeechReg />
       VideoCall
     </div>
   )
