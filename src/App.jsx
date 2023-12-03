8ac565cd30a51e69fcd8b039c5580087de95f6b3
Viet Anh Hoang <hoangvietanh8203@gmail.com>
2023-12-03 16:08:04 +0700

    Update VideoCall

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-
 function App() {
   return (
     <>
--- src/pages/Videocall.jsx
+++ src/pages/Videocall.jsx
@@ -1,12 +1,9 @@
 import { MUINavBar } from '../components/MUINavBar'
-
 import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
-import {NewsContextProvider } from "../NewsContext";
-import Newspaper from "../components/Newspaper";
 import Container from '@mui/material/Container';
 import SpeechReg from '../components/SpeechReg';
-
+import { VideoRoom } from '../components/VideoRomm';
 
 function Videocall() {
   const handleAudio = () => { 
@@ -20,11 +17,27 @@ function Videocall() {
   useEffect(() => {
     handleAudio();
   }, []);
+  const [joined, setJoined] = useState(false);
   return (
     <div className='Videocall'>
       <MUINavBar />
       <SpeechReg />
-      VideoCall
+      <div className='call'>
+        {!joined && (
+          <button onClick={() => setJoined(true)}>
+            Join Room
+          </button>
+        )}
+
+        {joined && (
+          <>
+            <button onClick={() => setJoined(false)}>
+              To Lobby
+            </button>
+            <VideoRoom />
+          </>
+        )}
+      </div>
     </div>
   )
 }
