ad4aaed4da16d78c69b1b8be4f89917b186bd05c
vitkondz <vitanhv26@gmail.com>
2023-12-05 11:51:44 +0700

    css homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,6 +6,7 @@ import SpeechReg from "../components/SpeechReg";
 import { useEffect } from "react";
 import VolumeSetting from "../components/Volume";
 import {
+  Box,
   Button,
   Card,
   CardActions,
@@ -30,8 +31,109 @@ function Homepage() {
       <MUINavBar />
       <SpeechReg />
       <VolumeSetting />
+
+      {/* content of homepage */}
       <Container>
+        <Grid>
+          <Box
+            style={{
+              position: "relative",
+              height: "85vh",
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              marginBottom: "100px",
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundImage: "url(/src/assets/images/blind-man-cross.jpg)",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                opacity: 0.4,
+                zIndex: -1,
+              }}
+            />
+            <div
+              style={{
+                fontSize: "100px",
+                fontWeight: "bolder",
+                background: "-webkit-linear-gradient(#FE0101, #FED60C)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "0px",
+              }}
+            >
+              VAssist
+            </div>
+            <div
+              style={{
+                fontSize: "30px",
+                fontWeight: "bolder",
+                color: "green",
+                marginBottom: "200px",
+              }}
+            >
+              Connect the Vision, Connect the World
+            </div>
+          </Box>
+        </Grid>
         <Grid container spacing={3} className="py-5">
+          <Grid item xs={6}>
+            <Card sx={{ maxWidth: 500 }}>
+              <CardMedia
+                sx={{ height: 250 }}
+                image="https://cdn.hailocvn.com/wp-content/uploads/2020/11/huong-dan-su-dung-man-hinh-tuong-tac-3.jpg"
+                title="news"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Tương tác tiện lợi
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Website được thiết kế với giao diện thân thiện, dễ sử dụng,
+                  nhiều thao tác tương tác được tích hợp sẵn giúp tối ưu hóa
+                  trải nghiệm của người dùng suy giảm thị lực
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Tìm hiểu thêm</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card sx={{ maxWidth: 500 }}>
+              <CardMedia
+                sx={{ height: 250 }}
+                image="https://bizflyportal.mediacdn.vn/bizflyportal/1578/2428/2021/06/24/21/36/tuo16245237988998.jpg"
+                title="news"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  VSpeak - Trợ lý ảo
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Trợ lý ảo VSpeak giúp người dùng có thể tương tác với website
+                  bằng giọng nói và nhận được phản hồi ngay lập tức. VSpeak có
+                  thể giúp người dùng tìm kiếm thông tin, điều hướng trang web,
+                  ...
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Tìm hiểu thêm</Button>
+              </CardActions>
+            </Card>
+          </Grid>
           <Grid item xs={6}>
             <Card sx={{ maxWidth: 500 }}>
               <CardMedia
@@ -105,7 +207,7 @@ function Homepage() {
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  Mạng xã hội
+                  Mạng xã hội - VConnect
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                   Kết nối cộng đồng người khiếm thị, chia sẻ và bắt chuyện cùng
